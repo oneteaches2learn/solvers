03737ce4562c49761638f309f754c388e0130556
Tyler Fara <tylerfara@gmail.com>
2025-12-17 11:46:03 -0800

    added ManufacturedFunctions1d_elliptic.m

---
+++ 1d_galerkin/objects/ManufacturedFunctions1d/ManufacturedFunctions1d_poisson.m
@@ -0,0 +1,126 @@
+classdef ManufacturedFunctions1d_poisson < ManufacturedFunctions1d_elliptic
+
+	properties
+		r
+		dr_du
+		BCtypes
+	end
+
+	methods
+		function self = ManufacturedFunctions1d_poisson(k,r,uTrue,BCtypes,NameValueArgs)
+
+			arguments
+				k
+				r
+				uTrue
+				BCtypes = '';
+				NameValueArgs.u_N = 0;
+				NameValueArgs.alpha_R = 0;
+				NameValueArgs.u_R = 0;
+			end
+			
+			% call superclass constructor
+			self@ManufacturedFunctions1d_elliptic(k,uTrue,NameValueArgs);
+
+			% store additional coefficients
+			self.r = r;
+
+			% compute derivatives of nonlinear functions
+			if Coefficients.isNonlinear(r)
+				u = sym('u','real');
+				self.dr_du = diff(r,u);
+			else
+				self.dr_du = 0;
+			end
+
+			% if boundary conditions are provided, store them
+			if ~isempty(BCtypes)
+				self.BCtypes = BCtypes;
+			end
+
+			% manufacture RHS
+			self.f = self.manufactureRHS; 
+
+		end
+
+
+		% MANUFACTURING FUNCTIONS
+		function f = manufactureRHS(self)
+		% Manufactures symfun f (i.e. the right-hand side) from data
+
+			% manufacture RHS
+			syms x; syms t;
+
+			% note: this is a kludge. The linear and nonlinear solvers make
+			% different assumptions about how the reaction term is handled. So
+			% that leads to different methods of manufacting the RHS. 
+			if Coefficients.isNonlinear(self.r)
+				f = self.divq + compose(self.r,self.uTrue);
+			else
+				f = self.divq + self.r * self.uTrue;
+			end
+
+		
+		end
+
+
+		% OUTPUTTER FUNCTIONS
+		function funcs = functionHandles(self)
+
+			% call superclass method
+			funcs = functionHandles@ManufacturedFunctions1d(self);
+
+			% store additional functions
+			syms x;
+			funcs.cofs.r = matlabFunction(symfun(self.r,x));
+
+			% store derivatives of nonlinear functions
+			funcs.cofs.dr_du = matlabFunction(symfun(self.dr_du,x));
+
+		end
+
+		function funcs = outputCoefficients(self)
+		% Alias for functionHandles method
+
+			funcs = self.functionHandles;
+
+		end
+
+		function BCs = outputBoundaryConditions(self,dom,BCtypes)
+
+			% store variables
+			% input handling
+			if nargin < 3
+				BCtypes = self.BCtypes;
+			end
+
+			% loop over boundary types
+			for i = 1:length(BCtypes)
+
+				% assign Dirichlet BC
+				if BCtypes(i) == 'D'
+					BCvals{i} = matlabFunction(symfun(self.uTrue, sym('x','real')));
+				end
+
+				% assign Neumann BC
+				if BCtypes(i) == 'N'
+					% determine outward normal
+					if i == 1, n = -1; else, n = 1; end
+
+					% compute Neumann BC value
+					bc = n * self.q;
+					BCvals{i} = matlabFunction(symfun(bc, sym('x','real')));
+				end
+					
+			end
+
+			% create Boundary1d object
+			BCs = Boundary1d(dom.xLim,BCtypes,BCvals);
+
+			% for manufactured BCs, d/du is zero
+			BCs.bConditions_du = {0,0};
+
+		end
+
+	end
+end
