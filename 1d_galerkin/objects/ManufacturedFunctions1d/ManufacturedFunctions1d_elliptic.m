813a3c0ead18a053d8c708bd6ca272a68c0954a2
Tyler Fara <tylerfara@gmail.com>
2025-12-17 11:45:39 -0800

    added ManufacturedFunctions1d_poisson.m

---
+++ 1d_galerkin/objects/ManufacturedFunctions1d/ManufacturedFunctions1d_elliptic.m
@@ -0,0 +1,15 @@
+classdef ManufacturedFunctions1d_elliptic < ManufacturedFunctions1d
+
+	properties
+	end
+
+	methods
+		function self = ManufacturedFunctions1d_elliptic(k,uTrue,NameValueArgs)
+			
+			% call superclass constructor
+			self@ManufacturedFunctions1d(k,uTrue,NameValueArgs);
+
+		end
+	end
+
+end
